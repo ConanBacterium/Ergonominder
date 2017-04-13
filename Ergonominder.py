51327adf128b61531f9541a054707a2abb29bf2f
Jacob Rohde <jas.rohde@gmail.com>
2017-04-13 13:07:45 +0200

    first commit

---
+++ Ergonominder.py
@@ -0,0 +1,14 @@
+import setproctitle
+import Tkinter as tk
+import tkMessageBox
+import time
+
+setproctitle.setproctitle("ergonominder")
+
+root = tk.Tk()
+
+while (True):
+    time.sleep(1200)
+    root.withdraw()
+    tkMessageBox.showwarning('Change position!', 'You\'e been static for too long! Change position + movement')
+    root.update()
